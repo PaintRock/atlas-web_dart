5b73705d5aa10c48c0822dcfca51cbdb9ca50384
Adel Knode <ann.knode@atlasschool.com>
2024-06-20 14:33:40 +0000

    Agent

--- 0x01-dart-function/4-outer_inner.dart
+++ 0x01-dart-function/4-outer_inner.dart
@@ -1,10 +1,22 @@
-void outer(String name, String id) {
-  void inner() {
-    print('Name: $name\nID: $id');
+String outer(String name, String id) {
+  List<String> words = name.split(' ');
+  if (words.length < 2) {
+    return name; // Return original name if it doesn't have at least two words
   }
-  inner();
+  
+  String firstWord = words[0];
+  String secondWord = words[1];
+
+  String lastInitial = secondWord[0];
+  String modifiedFirstWord = "$lastInitial.$firstWord";
+
+  words[0] = modifiedFirstWord;
+  words.removeAt(1);
+
+  return 'Hello Agent $modifiedFirstWord your id is $id';
 }
 
-// void main() {
-//   outer("Youssef Belhadj", "001");
-// }
+void main() {
+  String result = outer("Youssef Belhadj", "001");
+  print(result);
+}
