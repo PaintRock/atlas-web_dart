747e7434da1179bfcf73654620067fefb4108f69
Adel Knode <ann.knode@atlasschool.com>
2024-06-20 18:27:18 +0000

    you

--- 0x01-dart-function/7-basketball-shots.dart
+++ 0x01-dart-function/7-basketball-shots.dart
@@ -1,8 +1,8 @@
 // import '7-main.dart';
 
 int whoWins(Map<String, int> teamA, Map<String, int> teamB) {
-  int teamAScore = (teamA['Free throws']?? 0) * 1 + (teamA['2-pointers']?? 0) * 2 + (teamA['3-pointers']?? 0) * 3;
-  int teamBScore = (teamB['Free throws']?? 0) * 1 + (teamB['2-pointers']?? 0) * 2 + (teamB['3-pointers']?? 0) * 3;
+  int teamAScore = (teamA['Free throws']?? 0) + (teamA['2-pointers']?? 0) * 2 + (teamA['3-pointers']?? 0) * 3;
+  int teamBScore = (teamB['Free throws']?? 0) + (teamB['2-pointers']?? 0) * 2 + (teamB['3-pointers']?? 0) * 3;
 
   if (teamAScore > teamBScore) {
     return 1;
@@ -12,9 +12,18 @@ int whoWins(Map<String, int> teamA, Map<String, int> teamB) {
     return 0;
   }
 }
-main() {
-  var teamA = {'Free throws' : 23, '2 pointers' : 32, '3 pointers': 16};
-  var teamB = {'Free throws' : 31, '2 pointers' : 27, '3 pointers': 13};
-
+void main() {
+  var teamA = {'Free throws': 23, '2-pointers': 32, '3-pointers': 16};
+  var teamB = {'Free throws': 31, '2-pointers': 27, '3-pointers': 13};
   print(whoWins(teamA, teamB));
+
+  // Test case for a tie
+  var teamC = {'Free throws': 10, '2-pointers': 20, '3-pointers': 10};
+  var teamD = {'Free throws': 10, '2-pointers': 20, '3-pointers': 10};
+  print(whoWins(teamC, teamD));
+
+  // Test case for Team A winning
+  var teamE = {'Free throws': 20, '2-pointers': 30, '3-pointers': 15};
+  var teamF = {'Free throws': 15, '2-pointers': 25, '3-pointers': 10};
+  print(whoWins(teamE, teamF));
 }
