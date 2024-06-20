1ad24d7b56193c3345bb11926adeb3c58d0fd305
Adel Knode <ann.knode@atlasschool.com>
2024-06-20 15:26:01 +0000

    inner

--- 0x01-dart-function/4-outer_inner.dart
+++ 0x01-dart-function/4-outer_inner.dart
@@ -1,20 +1,10 @@
-String outer(String name, String id) {
-  String inner() {
-    List<String> words = name.split(' ');
-    
-    String firstWord = words[0];
-    String secondWord = words[1];
-
-    String lastInitial = secondWord[0];
-    String modifiedFirstWord = "$lastInitial.$firstWord";
-
-    return 'Hello Agent $modifiedFirstWord your id is $id';
+void outer(String name, String id) {
+  void inner() {
+    print('Name: $name\nID: $id');
   }
-  
-  return inner();
+  inner();
 }
 
 void main() {
-  String result = outer("Youssef Belhadj", "001");
-  print(result);
+  outer("Youssef Belhadj", "001");
 }
