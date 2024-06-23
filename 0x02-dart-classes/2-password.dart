0ee0a2cf00943e4a51ec17eb4a37a844339ae5ea
Adel Knode <ann.knode@atlasschool.com>
2024-06-23 20:14:51 +0000

    2v2

--- 0x02-dart-classes/2-password.dart
+++ 0x02-dart-classes/2-password.dart
@@ -1,11 +1,34 @@
 class Password {
-  String password;
-  Password(this.password);
+  String password = '';
+
+  Password();
 
   bool isValid() {
-    if (password.length < 8 || password.length > 16 || !password.contains(RegExp(r'[0-9]')) || !password.contains(RegExp(r'[a-z]')) || !password.contains(RegExp(r'[A-Z]')) || !password.contains(RegExp(r'[!@#%^&*]')){
-      return false;
-    }
-    return true;
+    return password.length >= 8 &&
+           password.length <= 16 &&
+           password.contains(RegExp(r'[0-9]')) &&
+           password.contains(RegExp(r'[a-z]')) &&
+           password.contains(RegExp(r'[A-Z]')) &&
+           password.contains(RegExp(r'[!@#$%^&*(),.?":{}|<>]'));
   }
+
+  @override
+  String toString() {
+    return 'Your Password is: $password';
+  }
+}
+void main() {
+  final ps = Password();
+  ps.password = "Just3z";
+  if (ps.isValid() == true)
+    print("${ps.password} is a Valid Password");
+  else
+    print("${ps.password} is Not a Valid Password");
+  print("${ps.toString()}");
+  ps.password = "ShouldWorkf7ne";
+  if (ps.isValid() == true)
+    print("${ps.password} is a Valid Password");
+  else
+    print("${ps.password} is Not a Valid Password");
+  print("${ps.toString()}");
 }
