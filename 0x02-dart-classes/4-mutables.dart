50442ea49030a3acd197a67b018c906931b30c90
Adel Knode <ann.knode@atlasschool.com>
2024-06-23 20:29:24 +0000

    3v3

---
+++ 0x02-dart-classes/4-mutables.dart
@@ -0,0 +1,26 @@
+class Password {
+  String _password = '';
+
+  Password({required String password}) : _password = password;
+
+  bool isValid() {
+    return _password.length >= 8 &&
+           _password.length <= 16 &&
+           _password.contains(RegExp(r'[0-9]')) &&
+           _password.contains(RegExp(r'[a-z]')) &&
+           _password.contains(RegExp(r'[A-Z]'));
+  }
+//tetter
+  String get password {
+    return _password;
+  }
+//setter
+  set password(String newPassword) {
+    _password = newPassword;
+  }
+
+  @override
+  String toString() {
+    return 'Your Password is: $_password';
+  }
+}
