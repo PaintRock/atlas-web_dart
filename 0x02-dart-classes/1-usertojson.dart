82ee686357a3d452f609bc4b6a9b3a4b97b1cb95
Adel Knode <ann.knode@atlasschool.com>
2024-06-23 19:40:39 +0000

    5v2

--- 0x02-dart-classes/1-usertojson.dart
+++ 0x02-dart-classes/1-usertojson.dart
@@ -22,14 +22,23 @@ class User {
       height: userJson['height'],
     );
   }
+  @override
+  String toString() {
+    return 'User{name: $name, age: $age, height: $height, id: $id}';
+  }
 }
 void main() {
   final user = User(name: "Youssef", age: 25, height: 1.89);
   print(user.toJson());
 }
 
-@override
-  String toString() {
-    return 'User{name: $name, age: $age, height: $height, id: $id}';
-  }
-  
+
+
+  void main() {
+  final djo = User(id: 1, name: "Djo", age: 25, height: 1.89);
+    print(djo.toJson());
+  Map map = {'id': 3, 'name': 'Youssef', 'age': 26, 'height': 1.9};
+  final youssef = User.fromJson(map);
+  print(djo.toString());
+  print(youssef.toString());
+}
